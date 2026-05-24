534c14e8c787e7fab123cd90997cd74149f11cad
Shahinul99 <rumaisaabrar99@gmail.com>
2026-05-23 20:38:19 -0400

    update database file

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,7 +7,7 @@ const { Pool } = pg;
 
 // Using the native pool connection via connectionString
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:"postgresql://neondb_owner:npg_kyNwgar7Soq9@ep-twilight-shape-ap6dzmj6-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
 // A quick helper to test the connection health on server startup
